6ca3d5ffc30beed0f7fc5da1fe49688e5dee491d
swellwrench <swellwrench@gmail.com>
2020-04-02 18:57:52 -0500

    Studio 2

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -18,4 +18,13 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
 }
